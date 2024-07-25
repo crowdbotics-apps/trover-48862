f2a875048f86fc420b1da9c13bae880100ac4807
Crowdbotics <team@crowdbotics.com>
2024-07-25 18:01:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trover-48862",
+  url: "https://trover-48862.botics.co",
+  api: "https://trover-48862.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
